bb0294e6ea854e09b6c75832ee4c50b1462c466c
Hugh Kim <hoyeon222@gmail.com>
2024-02-12 03:43:36 +0000

    2024-02-12_03-43-29-test 15

---
+++ content/posts/kubernetes/kubernetes-study-15-test.md
@@ -0,0 +1,6 @@
+---
+title: "Kubernetes Study 15 Test"
+date: 2024-02-12T03:43:24Z
+draft: true
+---
+
--- pub.sh
+++ pub.sh
@@ -2,6 +2,11 @@
 
 # Current date and time
 date_time=$(date +"%Y-%m-%d_%H-%M-%S")
+current_dir=$pwd
+
+echo "Enter commit message: "
+read message
+commit_message="$date_time-$message" 
 
 # 1. Build Hugo site
 hugo
@@ -9,28 +14,17 @@ hugo
 # 2. Move to 'public' folder
 cd public
 
-# 3. User input commit message
-echo "Enter commit message: "
-read commit_message
-
 # 4. Add changes and commit
 git add .
 git commit -m "$commit_message"
+git push origin master
 
 # 5. Move back to project root
 cd ../
 
-# 6. Update submodule
-git submodule update --init --recursive
-
 # 7. Add changes and commit
 git add .
-git commit -m "Submodule update - $date_time"
-
-# 8. Deploy to Github Pages
-git push -u origin master --recurse-submodules=on-demand
-
-# 9. Restore working directory
-cd $current_dir
+git commit -m "$commit_message"
+git push origin master
 
 echo "Deployment complete!"
